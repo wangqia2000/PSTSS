cb23d4cf59b51a62e63c5aee5f6c0f664e214a76
tinyriver <53994050+wangqia2000@users.noreply.github.com>
2024-05-16 11:25:49 +0800

    Update demo.sh

--- demo.sh
+++ demo.sh
@@ -3,16 +3,16 @@
 rm -rf output
 mkdir output
 
-echo [DEMO] running MoSSo
-./run.sh example_graph.txt MoSSo.txt mosso 3 120 1000 > output/MoSSo.log
+echo [DEMO] running MoSSo-base
+./run.sh example_graph.txt MoSSo.txt mosso 3 120 1000 > output/base.log
 
-echo [DEMO] running MoSSo-Simple
-./run.sh example_graph.txt MoSSoSimple.txt simple 3 120 1000 > output/MoSSoSimple.log
+echo [DEMO] running Simple
+./run.sh example_graph.txt Simple.txt simple 3 120 1000 > output/Simple.log
 
-echo [DEMO] running MoSSo-MCMC
-./run.sh example_graph.txt MoSSoMCMC.txt mcmc 1000 > output/MoSSoMCMC.log
+echo [DEMO] running PSTSS
+./run.sh example_graph.txt PSTSSM.txt mcmc 1000 > output/PSTSS.log
 
-echo [DEMO] running MoSSo-Greedy
-./run.sh example_graph.txt MoSSoGreedy.txt sgreedy 1000 > output/MoSSoGreedy.log
+echo [DEMO] running SGreedy
+./run.sh example_graph.txt Greedy.txt sgreedy 1000 > output/Greedy.log
 
 echo [FINISHED] The results and logs are stored in the "output" folder.
